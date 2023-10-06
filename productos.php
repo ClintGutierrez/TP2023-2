4b4bcf8ff3e94a35b8893bc53127f77207261c01
ClintGutierrez <clint.gutierrez@urp.edu.pe>
2023-10-05 19:44:54 -0500

    cambio v2

--- productos.php
+++ productos.php
@@ -4,7 +4,7 @@ require_once "vistas/parte_superior.php"
 <!--INICIO CONT PRINCIPAL-->
 <div class="container">
     <h1>Gestionar Productos</h1> 
-    hola
+    
 </div>
 <!--FIN DEL CONTENIDO PRINCIPAL-->
 <?php
