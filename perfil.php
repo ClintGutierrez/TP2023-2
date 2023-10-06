85cd75af8c87d056f321ebdcb617775e2aff88f9
ClintGutierrez <clint.gutierrez@urp.edu.pe>
2023-10-05 19:53:39 -0500

    modificacion perfil

--- perfil.php
+++ perfil.php
@@ -24,24 +24,32 @@ require_once "vistas/parte_superior.php"
                         <div class="modal-dialog">
                             <div class="modal-content">
                             <div class="modal-header">
-                                <h1 class="modal-title fs-5" id="exampleModalLabel">New message</h1>
+                                <h1 class="modal-title fs-5" id="exampleModalLabel">Editar Perfil</h1>
                                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                             </div>
                             <div class="modal-body">
                                 <form>
                                 <div class="mb-3">
-                                    <label for="recipient-name" class="col-form-label">Recipient:</label>
+                                    <label for="recipient-name" class="col-form-label">Nombre</label>
                                     <input type="text" class="form-control" id="recipient-name">
                                 </div>
                                 <div class="mb-3">
-                                    <label for="message-text" class="col-form-label">Message:</label>
+                                    <label for="message-text" class="col-form-label">Email</label>
+                                    <textarea class="form-control" id="message-text"></textarea>
+                                </div>
+                                <div class="mb-3">
+                                    <label for="message-text" class="col-form-label">Teléfono</label>
+                                    <textarea class="form-control" id="message-text"></textarea>
+                                </div>
+                                <div class="mb-3">
+                                    <label for="message-text" class="col-form-label">Dirección</label>
                                     <textarea class="form-control" id="message-text"></textarea>
                                 </div>
                                 </form>
                             </div>
                             <div class="modal-footer">
                             <input type=button class="btn btn-primary" name=boton value=Cerrar data-dismiss="modal">
-                                <button type="button" class="btn btn-primary">Send message</button>
+                                <button type="button" class="btn btn-primary">Aceptar</button>
                             </div>
                             </div>
                         </div>
